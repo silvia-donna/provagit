ff1f13526b3d76758303053cdb231510927dd5c3
Silvia <Silvia@LAPTOP-C2KHPGDJ>
2021-03-08 09:38:56 +0100

    modifica 0

--- src/main/java/it/polito/tdp/provagit/FXMLController.java
+++ src/main/java/it/polito/tdp/provagit/FXMLController.java
@@ -4,22 +4,32 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
-public class FXMLController implements Initializable {
-    
+public class FXMLController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button button;
+
     @FXML
     private Label label;
-    
+
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    void handleButtonAction(ActionEvent event) {
+
     }
-    
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+
+    @FXML
+    void initialize() {
+        assert button != null : "fx:id=\"button\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert label != null : "fx:id=\"label\" was not injected: check your FXML file 'Scene.fxml'.";
+
+    }
 }
